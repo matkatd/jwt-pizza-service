5d749754d216854193af1200fc88c6fe678ecbea
David T <matkatdavid@gmail.com>
2024-09-25 22:23:45 -0600

    test: logout

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,8 +1,24 @@
 const request = require("supertest");
 const app = require("../service");
+const { Role, DB } = require("../database/database");
 
 const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
 let testUserAuthToken;
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+  let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + "@admin.com";
+
+  await DB.addUser(user);
+
+  user.password = "toomanysecrets";
+  return user;
+}
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
@@ -19,6 +35,35 @@ test("login", async () => {
 
   const { password, ...user } = { ...testUser, roles: [{ role: "diner" }] };
   expect(loginRes.body.user).toMatchObject(user);
-  console.log(password);
   expect(password).toBe("a");
+});
+
+test("logout", async () => {
+  const logoutRes = await request(app)
+    .delete("/api/auth")
+    .set("Authorization", `Bearer ${testUserAuthToken}`);
+
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe("logout successful");
+  expect(logoutRes.headers.authorization).toBeUndefined();
+});
+
+test("update user", async () => {
+  if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+    jest.setTimeout(60 * 1000 * 5); // 5 minutes
+  }
+  const user = await createAdminUser();
+  const loginRes = await request(app).put("/api/auth").send(user);
+  testUserAuthToken = loginRes.body.token;
+  const updatedUser = {
+    ...loginRes.body.user,
+    email: Math.random().toString(36).substring(2, 12) + "@test.com",
+  };
+  const updateUserRes = await request(app)
+    .put(`/api/auth/${updatedUser.id}`)
+    .set("Authorization", `Bearer ${testUserAuthToken}`)
+    .send(updatedUser);
+
+  expect(updateUserRes.status).toBe(200);
+  expect(updateUserRes.body.email).toBe(updatedUser.email);
 });
